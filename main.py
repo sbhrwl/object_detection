164b29d1c1c252887d048967faae3d526c457950
sbhrwl <sabharwalonnet@gmail.com>
2021-05-27 14:45:35 +0300

    read image

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hi")
+import cv2
+
+if __name__ == '__main__':
+    print(cv2.__version__)
+    img = cv2.imread('inputs/image.jpg', -1)
+    cv2.imshow('image', img)
+    pressed_key = cv2.waitKey(0)
+    if pressed_key == 27:  # Escape Key
+        cv2.destroyAllWindows()
