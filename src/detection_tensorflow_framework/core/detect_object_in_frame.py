9481c01f44d803c658e5dc7f2b508908c3ad3737
sbhrwl <sabharwalonnet@gmail.com>
2021-06-15 14:00:58 +0300

    Circle added in utils
    # Each bounding box consists of 5 predictions: x, y, w, h, and confidence

--- src/detection_tensorflow_framework/core/detect_object_in_frame.py
+++ src/detection_tensorflow_framework/core/detect_object_in_frame.py
@@ -1,6 +1,7 @@
 
 
 def get_detection_details(predicted_boundary_box):
+    # Each bounding box consists of 5 predictions: x, y, w, h, and confidence
     for key, value in predicted_boundary_box.items():
         predicted_confidence = value[:, :, 4:]
         boxes = value[:, :, 0:4]
